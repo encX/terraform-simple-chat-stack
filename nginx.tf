77a05a3b2de859a0a3c6d9c151750e2a33ab9064
Plai <plai.rt.w@gmail.com>
2024-12-17 18:50:30 +0700

    Update nginx.tf

--- nginx.tf
+++ nginx.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginx" {
   name = "nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart = "ingress-nginx"
-  namespace  = kubernetes_namespace.nginx-ns.metadata[0].name
+  namespace  = kubernetes_namespace.nginx.metadata[0].name
 
   set {
     name  = "controller.publishService.enabled"
